26cd17c7e93600c81087f20aaf9f60f960d1eb1e
Michael Light <mklight06@gmail.com>
2022-01-20 14:17:42 -0700

    created cocktails component

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import Cocktails from './Cocktails';
 
 export default function App() {
+  const { initialCocktails, setInitialCocktails } = useRef(false);
+  const { cocktailData, setCocktailData } = useState([]);
   return (
     <div>
       <div id="welcome">Welcome!</div>
@@ -8,6 +11,14 @@ export default function App() {
         Lets get started finding a cocktail! Start by picking a
         base from the drop down below
 
+      </div>
+      <div id="cocktails">
+        {initialCocktails ? cocktailData.map((cocktail) => (
+          <div>
+            <Cocktails cocktail={cocktail} />
+          </div>
+        ))
+          : null}
       </div>
     </div>
   );
---
+++ client/src/Components/Cocktails.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Cocktails() {
+  return (
+    <div>
+      cocktails
+    </div>
+  );
+}
